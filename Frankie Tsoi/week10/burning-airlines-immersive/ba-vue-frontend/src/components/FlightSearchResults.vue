4ab73a6a9b0f4981d42c797687907c10989d6c9c
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-02-28 21:59:28 +1100

    vue frontend

---
+++ Frankie Tsoi/week10/burning-airlines-immersive/ba-vue-frontend/src/components/FlightSearchResults.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h2>Search Results from {{origin}} to {{destination}}</h2>
+
+    <div v-if='loading' class="load">
+      <p>Loading flight results......</p>
+    </div>
+
+    <div v-else>
+
+
+      <div v-for="f in flights">
+        <p>{{ f.flight_number }}</p>
+
+      </div>
+
+    </div>
+
+    </div>
+    
+</template>
+
+<script>
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000/'
+
+export default {
+  name: 'FlightSearchResults',
+  props: ['origin', 'destination'],
+  data(){
+    return{
+      flight:[],
+      loading: true,
+      error: null
+
+    }; //return
+  },//data
+  
+    async mounted(){
+      console.log('component mounted', this.origin, this.destination);
+
+      try{
+        const url = `${API_BASE_URL}/flights/search/${this.origin}/${this.destination}`
+        const res = await axios.get(url);
+        this.flights = res.data;
+        this.loading = false;
+        console.log('response', res.data)
+      } catch(err){
+        console.log('Error loading flight search results', err);
+        this.error = error;
+      }
+
+
+    },//mounted
+
+    methods: {
+
+    }//method
+ 
+}//export defaults
+</script>
+
+<style>
+
+</style>
